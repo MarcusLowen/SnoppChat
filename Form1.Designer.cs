228f7760e85fa24ff2a2f9902977335271eeca2e
MarcusLowen <74663178+MarcusLowen@users.noreply.github.com>
2020-11-18 15:18:30 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ChattFönster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Skicka = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.SuspendLayout();
+            // 
+            // Skicka
+            // 
+            this.Skicka.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Skicka.Location = new System.Drawing.Point(701, 419);
+            this.Skicka.Name = "Skicka";
+            this.Skicka.Size = new System.Drawing.Size(75, 20);
+            this.Skicka.TabIndex = 1;
+            this.Skicka.Text = "Send";
+            this.Skicka.UseVisualStyleBackColor = true;
+            this.Skicka.Click += new System.EventHandler(this.Skicka_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 419);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(683, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.vScrollBar1.Location = new System.Drawing.Point(765, 9);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(26, 407);
+            this.vScrollBar1.TabIndex = 3;
+            this.vScrollBar1.Value = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Skicka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Skicka;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+    }
+}
+
